eb3dbbe76043930c5cf2e29bdfaf03e052c70f02
JoSeongJeong <jsj046@naver.com>
2023-08-09 20:45:15 +0900

    [Add] BJ2743.java

---
+++ src/BJ2743.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BJ2743 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String S = scan.next();
+
+        System.out.println(S.length());
+    }
+}
