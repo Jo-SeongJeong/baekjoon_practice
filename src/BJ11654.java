cf859b81d2b107f2aeca235c1587c3cf5c3c85e0
JoSeongJeong <jsj046@naver.com>
2023-08-10 16:43:09 +0900

    [Add] BJ11654.java

---
+++ src/BJ11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BJ11654 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int ch = scan.next().charAt(0);
+
+        System.out.println(ch);
+    }
+}
