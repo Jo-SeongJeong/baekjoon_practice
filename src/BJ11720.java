e455408747693921302c8c02a8b45c0fbb4d916f
JoSeongJeong <jsj046@naver.com>
2023-08-10 16:44:00 +0900

    [Add] BJ11720.java

---
+++ src/BJ11720.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BJ11720 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        String num = scan.next();
+        int sum = 0;
+
+        for(int i=0; i<N; i++) {
+            sum += num.charAt(i) - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
