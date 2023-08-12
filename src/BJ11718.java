0266fdc2dedf7826d7b35cc9892c20ac36d31065
JoSeongJeong <jsj046@naver.com>
2023-08-12 20:39:19 +0900

    [Add] BJ11718.java

---
+++ src/BJ11718.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BJ11718 {
+    public static void main (String []args) {
+        Scanner scan = new Scanner(System.in);
+
+        while(scan.hasNext()) {
+            System.out.println(scan.nextLine());
+        }
+    }
+}
