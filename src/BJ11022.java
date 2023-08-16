00170a406de8cfc35e33e77add47a0234a10cde8
JoSeongJeong <jsj046@naver.com>
2023-08-17 01:30:10 +0900

    [Add] BJ11022.java

---
+++ src/BJ11022.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BJ11022 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+
+        for(int i=1; i<=T; i++) {
+            int sum = 0;
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+            sum = A + B;
+            System.out.println("Case #"+i+": "+A+" + "+B+" = "+sum);
+        }
+    }
+}
