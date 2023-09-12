b495d478b4ee52d5030951121a9074649747585a
JoSeongJeong <jsj046@naver.com>
2023-09-12 16:31:08 +0900

    [Add] BJ24266.java

---
+++ src/BJ24266.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BJ24266 {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        long n = scan.nextInt();
+        System.out.println(n*n*n);
+        System.out.println(3);
+    }
+}
