c8da76a58093de0486eb284b0d7ba6fb485ab5b0
JoSeongJeong <jsj046@naver.com>
2023-09-17 16:22:26 +0900

    [Fix] BJ2231.java

--- src/BJ2231.java
+++ src/BJ2231.java
@@ -4,16 +4,22 @@ public class BJ2231 {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        int m = n;
-        int sum = 0;
-        while(m>10) {
-            sum += m % 10;
-            m = m/10;
-            if(m < 10) {
-                sum += m;
+        int result = 0;
+
+        for(int i=0; i<n; i++) {
+            int num = i;
+            int sum = 0;
+
+            while(num != 0) {
+                sum += num % 10;
+                num = num / 10;
+            }
+
+            if(sum+i == n) {
+                result = i;
                 break;
             }
         }
-        System.out.println(n-sum);
+        System.out.println(result);
     }
 }
