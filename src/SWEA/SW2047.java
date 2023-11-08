c6d3a06c8a5efd03f4fbb62cbeb8a38589dd24a5
JoSeongJeong <jsj046@naver.com>
2023-11-08 18:31:01 +0900

    [Add] SW2047.java

---
+++ src/SWEA/SW2047.java
@@ -0,0 +1,12 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SW2047 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        str = str.toUpperCase();
+        System.out.println(str);
+    }
+}
