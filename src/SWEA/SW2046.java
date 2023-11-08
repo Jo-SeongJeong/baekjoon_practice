cc7bfb614d86dcf2e93755e4e83c3c51582ca07b
JoSeongJeong <jsj046@naver.com>
2023-11-08 18:31:28 +0900

    [Add] SW2046.java

---
+++ src/SWEA/SW2046.java
@@ -0,0 +1,14 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SW2046 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for(int i=0; i<n; i++) {
+            System.out.print("#");
+        }
+    }
+}
