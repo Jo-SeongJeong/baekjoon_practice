2e72f7024603bd80a448113d04480fc7ac508040
JoSeongJeong <jsj046@naver.com>
2023-11-13 20:24:28 +0900

    [Add] SW1938.java

---
+++ src/SWEA/SW1938.java
@@ -0,0 +1,16 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class SW1938 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
