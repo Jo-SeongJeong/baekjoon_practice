a114bd3748112714769d8a336fb9f5631a008a54
JoSeongJeong <jsj046@naver.com>
2024-01-03 21:05:22 +0900

    [Refactor] SWEA1204.java

--- src/SWEA/SW1204.java
+++ src/SWEA/SW1204.java
@@ -3,34 +3,28 @@ package SWEA;
 import java.util.Scanner;
 
 public class SW1204 {
-    public static void main(String[] args) {
+    public static void main(String args[]) throws Exception {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
 
-        for(int i=1; i<=t; i++) {
+        for (int i = 1; i <= t; i++) {
             int n = scan.nextInt();
-            int []array = new int[1000];
-
-            for(int j=0; j<1000; j++) {
-                array[j] = scan.nextInt();
-            }
-
+            int[] array = new int[101];
             int max = 0;
-            int freq = 0;
+            int result = 0;
 
-            for(int k=0; k<1000; k++) {
-                int count = 0;
-                for(int l=0; l<1000; l++) {
-                    if(array[k] == array[l]) {
-                        count++;
-                    }
-                }
-                if(count >= max) {
-                    max = count;
-                    freq = array[k];
+            for (int j = 0; j < 1000; j++) {
+                int score = scan.nextInt();
+                array[score]++;
+            }
+
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] >= max) {
+                    max = array[j];
+                    result = j;
                 }
             }
-            System.out.println("#"+i+" "+freq);
+            System.out.println("#" + n + " " + result);
         }
     }
 }
