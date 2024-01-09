9abb693fc12db85b70b1e3ffcce16911cbf10c52
JoSeongJeong <jsj046@naver.com>
2024-01-09 21:33:23 +0900

    [Refactor] SW1979.java

--- src/SWEA/SW1979.java
+++ src/SWEA/SW1979.java
@@ -3,8 +3,58 @@ package SWEA;
 import java.util.Scanner;
 
 public class SW1979 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
 
+        for (int i = 1; i <= t; i++) {
+            int n = scan.nextInt();
+            int k = scan.nextInt();
+            int[][] array = new int[n][n];
+
+            for (int j = 0; j < n; j++) {
+                for (int l = 0; l < n; l++) {
+                    array[j][l] = scan.nextInt();
+                }
+            }
+
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                for (int l = 0; l < n; l++) {
+                    int c_temp = 0;
+                    if (array[j][l] == 1 && c_temp == 0) {
+                        for (int c = l; c < n; c++) {
+                            l = c;
+                            if (array[j][c] == 0) {
+                                break;
+                            }
+                            c_temp++;
+                        }
+                        if (k == c_temp) {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            for(int l = 0; l < n; l++) {
+                for(int j = 0; j < n; j++) {
+                    int r_temp = 0;
+                    if(array[j][l] == 1 && r_temp == 0) {
+                        for(int r = j; r < n; r++) {
+                            j = r;
+                            if(array[r][l] == 0) {
+                                break;
+                            }
+                            r_temp++;
+                        }
+                        if(k == r_temp) {
+                            count++;
+                        }
+                    }
+                }
+            }
+            System.out.println("#" + i + " " + count);
+        }
     }
 }
